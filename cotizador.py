00d25717ef17a56afab1dc9d86ac25f32f1fdb48
Jcamilo2808 <142037550+Jcamilo2808@users.noreply.github.com>
2025-08-25 04:54:36 -0500

    Update cotizador.py

--- cotizador.py
+++ cotizador.py
@@ -7,7 +7,7 @@ from io import BytesIO
 st.set_page_config(page_title="COTIZADOR PRODUCTO ARTESANAL", page_icon="💎", layout="wide")
 
 # === (Opcional) Logo ===
-LOGO_PATH = "https://github.com/Jcamilo2808/Cotizador/blob/main/Diseno_sin_titulo-removebg-preview.png"
+LOGO_PATH = https://github.com/Jcamilo2808/Cotizador/blob/main/Diseno_sin_titulo-removebg-preview.png
 
 def encode_img_b64(path: str) -> str:
     try:
